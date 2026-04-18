8ed72891bff367010c0836c9167b4c4b8cfcca86
Adem Riahi <43847781+adem147@users.noreply.github.com>
2026-04-18 23:39:36 +0100

    new demo sceane with car driving

---
+++ Assets/CarControllerScript.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class CarEnterExit : MonoBehaviour
+{
+    public PrometeoCarController prometeoCarController;
+    public Transform exitPoint;
+
+    private GameObject player;
+    private bool isInside = false;
+
+    void Update()
+    {
+        if (isInside && Input.GetKeyDown(KeyCode.E))
+        {
+            EnterCar();
+        }
+        else if (!isInside && player != null && Input.GetKeyDown(KeyCode.F))
+        {
+            ExitCar();
+        }
+    }
+
+    void EnterCar()
+    {
+        player.GetComponent<playermvt>().enabled = false;
+        player.transform.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+        player.transform.SetParent(transform);
+       // player.SetActive(false); // hide player
+        prometeoCarController.enabled = true;
+
+        isInside = false;
+    }
+
+    void ExitCar()
+    {
+        player.GetComponent<playermvt>().enabled = true;
+        player.transform.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+        player.transform.SetParent(null);
+        exitPoint.position = new Vector3(5,0,0)+transform.position;
+        player.transform.position = exitPoint.position;
+      //  player.SetActive(true);
+
+        prometeoCarController.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hhhhhh");
+        if (other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            isInside = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInside = false;
+        }
+    }
+}
